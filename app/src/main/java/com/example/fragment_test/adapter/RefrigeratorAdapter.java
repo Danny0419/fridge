64fc2333eebc76bf0ab9f0dc5cd97dfccc068378
Danny <zxc0989992088@gmail.com>
2024-06-22 16:27:20 +0800

    食物管理頁面新增標籤滑動

--- app/src/main/java/com/example/fragment_test/adapter/RefrigeratorAdapter.java
+++ app/src/main/java/com/example/fragment_test/adapter/RefrigeratorAdapter.java
@@ -64,31 +64,4 @@ public class RefrigeratorAdapter extends RecyclerView.Adapter<RefrigeratorAdapte
     public int getItemCount() {
         return kinds.size();
     }
-
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//
-//        ViewHolder viewHolder = null;
-//        if (convertView == null) {
-//            viewHolder = new ViewHolder();
-//            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.kind_of_ingredient_container, parent, false);
-//
-//
-//            viewHolder.kind = convertView.findViewById(R.id.ingredientKindText);
-//            viewHolder.kindOfIngredientContainer = convertView.findViewById(R.id.kind_of_ingredient_container);
-//
-//            convertView.setTag(viewHolder);
-//        } else {
-//            viewHolder = (ViewHolder) convertView.getTag();
-//        }
-//
-//        viewHolder.kind.setText(kinds.get(position));
-//
-//        List<Ingredient> kindOfIngredient = ingredientMap.get(kinds.get(position));
-//        GridLayoutManager gridLayoutManager = new GridLayoutManager(parent.getContext(), 5);
-//        viewHolder.kindOfIngredientContainer.setLayoutManager(gridLayoutManager);
-//        viewHolder.kindOfIngredientContainer.setAdapter(new RefrigeratorKindAdapter(parent.getContext(),kindOfIngredient != null ? kindOfIngredient : new ArrayList<>()));
-//
-//        return convertView;
-//    }
 }
