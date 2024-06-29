f5314cdb56f5a47fce365227f0eb8c3c9abd255a
Danny <zxc0989992088@gmail.com>
2024-06-29 13:53:15 +0800

    新增RecipeIngredientAdapter類

---
+++ app/src/main/java/com/example/fragment_test/adapter/RecipeIngredientAdapter.java
@@ -0,0 +1,49 @@
+package com.example.fragment_test.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.fragment_test.R;
+import com.example.fragment_test.pojo.Ingredient;
+
+import java.util.ArrayList;
+
+public class RecipeIngredientAdapter extends RecyclerView.Adapter<RecipeIngredientAdapter.RecipeIngredientViewHolder> {
+
+    ArrayList<Ingredient> ingredients;
+
+    public class RecipeIngredientViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView ingredientImg;
+        public TextView ingredientName;
+
+        public RecipeIngredientViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ingredientImg = itemView.findViewById(R.id.ingredientImg);
+            ingredientName = itemView.findViewById(R.id.ingredientName);
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecipeIngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_ingredient_card_item, parent);
+        return new RecipeIngredientViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeIngredientViewHolder holder, int position) {
+        holder.ingredientImg.setText(ingredients.get(position).img);
+        holder.ingredientName.setText(ingredients.get(position).name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ingredients.size();
+    }
+}
