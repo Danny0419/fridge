05155de01cf32b8d5dad94ebc3dc487d3ff85cbe
吳侑城 <vyoeehans@gmail.com>
2024-07-06 19:13:35 +0800

    1. 新增px與dp轉換的類別 2. 加寫px與dp轉換的類別中的不知道可不可行的方法

--- app/src/main/java/com/example/fragment_test/ConcertSize.java
+++ app/src/main/java/com/example/fragment_test/ConcertSize.java
@@ -2,6 +2,11 @@ package com.example.fragment_test;
 
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class ConcertSize {
     /**
@@ -37,5 +42,49 @@ public class ConcertSize {
     public static float getDensity(Context context){
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
         return metrics.density;
+    }
+
+    // LinearLayout設定大小與位置
+    public static void setLinearSizeAndPosition(View view, int width, int height, int marginLeft, int marginTop, int marginRight, int marginEnd){
+        // 設定寬、高
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(width, height);
+
+        // 設定邊距
+        layoutParams.setMargins(marginLeft, marginTop, marginRight, marginEnd);
+
+        view.setLayoutParams(layoutParams);
+    }
+
+    // ConstraintLayout設定大小與位置
+    public static void setConstraintSizeAndPosition(View view, int width, int height, int marginLeft, int marginTop, int marginRight, int marginEnd){
+        // 設定寬、高
+        ConstraintLayout.LayoutParams  layoutParams=new ConstraintLayout.LayoutParams(width,height);
+
+        // 設定邊距
+        layoutParams.setMargins(marginLeft, marginTop, marginRight, marginEnd);
+
+        view.setLayoutParams(layoutParams);
+    }
+
+    // 實作轉換（px to dp）與設定大小與位置
+    public static void adjustViewSizeAndPosition(View view, float width, float height, float marginLeft, float marginTop, float marginRight, float marginEnd) {
+        Context context = view.getContext();
+
+        // px轉dp
+        width=convertPixelToDp(width,context);
+        height=convertPixelToDp(height,context);
+        marginLeft=convertPixelToDp(marginLeft,context);
+        marginTop=convertPixelToDp(marginTop,context);
+        marginRight=convertPixelToDp(marginRight,context);
+        marginEnd=convertPixelToDp(marginEnd,context);
+
+        // 判別被哪種layout所包裹
+        if (view.getLayoutParams() instanceof LinearLayout.LayoutParams){
+            setLinearSizeAndPosition(view, (int) width, (int) height, (int) marginLeft, (int) marginTop, (int) marginRight, (int) marginEnd);
+        } else if (view.getLayoutParams() instanceof ConstraintLayout.LayoutParams) {
+            setConstraintSizeAndPosition(view, (int) width, (int) height, (int) marginLeft, (int) marginTop, (int) marginRight, (int) marginEnd);
+        }
+
+
     }
 }
