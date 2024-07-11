ab3e03cf850f59d696d6af5abefa3134c3589686
Danny <zxc0989992088@gmail.com>
2024-07-11 17:04:51 +0800

    將actionbar改成toolbar

--- app/src/main/java/com/example/fragment_test/MainActivity2.java
+++ app/src/main/java/com/example/fragment_test/MainActivity2.java
@@ -37,7 +37,7 @@ public class MainActivity2 extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_refrigerator, R.id.navigation_recipe, R.id.navigation_schedule, R.id.navigation_shopping_list)
+                R.id.navigation_home, R.id.navigation_refrigerator, R.id.navigation_recipe, R.id.navigation_schedule, R.id.navigation_shopping_list, R.id.navigation_camera)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
         Toolbar toolbar = findViewById(R.id.toolbar_main);
@@ -57,7 +57,7 @@ public class MainActivity2 extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.scan) {
-            navController.navigate(R.id.navigation_camera);
+            navController.navigate(R.id.action_navigation_home_to_navigation_camera);
         }
         return true;
     }
