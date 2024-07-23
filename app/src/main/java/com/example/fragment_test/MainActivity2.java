518667408c451d2ac49385040d189ed6179f487d
Ｄanny0419 <zxc0989992088@gmail.com>
2024-07-23 17:59:54 +0800

    重構

--- app/src/main/java/com/example/fragment_test/MainActivity2.java
+++ app/src/main/java/com/example/fragment_test/MainActivity2.java
@@ -37,7 +37,8 @@ public class MainActivity2 extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_refrigerator, R.id.navigation_recipe, R.id.navigation_schedule, R.id.navigation_shopping_list, R.id.navigation_camera)
+                R.id.navigation_home, R.id.navigation_refrigerator, R.id.navigation_recipe,
+                R.id.navigation_schedule, R.id.navigation_shopping_list, R.id.navigation_camera)
                 .build();
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
         Toolbar toolbar = binding.toolbarMain;
