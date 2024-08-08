8b895f1dc4e16190bccb50e37ca8b6cf1b1ac820
吳侑城 <vyoeehans@gmail.com>
2024-08-09 02:06:58 +0800

    規劃頁面製作與按鈕點選動畫(很醜，有點怪，會再調)

--- app/src/main/java/com/example/fragment_test/adapter/ScheduleAdapter.java
+++ app/src/main/java/com/example/fragment_test/adapter/ScheduleAdapter.java
@@ -1,9 +1,14 @@
 package com.example.fragment_test.adapter;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -68,6 +73,48 @@ public class ScheduleAdapter extends BaseAdapter {
             recipe = new ArrayList<Recipe>();
         }
 //        viewHolder.foodContainer.setAdapter(new ADayOfScheduleAdapter(recipe));
+
+        /*點擊按鈕
+        選擇要吃甚麼的區塊下拉與收合
+        的動畫*/
+        //下拉
+        Button selectMealBtn = convertView.findViewById(R.id.select_meal_btn);
+        RelativeLayout readyToCook = convertView.findViewById(R.id.ready_to_cook);
+        TextView clickHint=convertView.findViewById(R.id.click_hint);
+        selectMealBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (readyToCook.getVisibility() == View.GONE) {
+                    ObjectAnimator slideDown = ObjectAnimator.ofFloat(readyToCook, "translationY", -readyToCook.getHeight(), 0f);
+                    slideDown.setDuration(500);
+                    slideDown.start();
+                    readyToCook.setVisibility(View.VISIBLE);
+                    clickHint.setVisibility(View.VISIBLE);
+                    selectMealBtn.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        //收合
+        View meal_today = convertView.findViewById(R.id.meal_today);
+        meal_today.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (readyToCook.getVisibility() == View.VISIBLE) {
+                    ObjectAnimator slideUp = ObjectAnimator.ofFloat(readyToCook, "translationY", 0f, -readyToCook.getHeight());
+                    slideUp.setDuration(500);
+                    slideUp.start();
+                    slideUp.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            readyToCook.setVisibility(View.GONE);
+                            clickHint.setVisibility(View.GONE);
+                            selectMealBtn.setVisibility(View.VISIBLE);
+                        }
+                    });
+                }
+            }
+        });
         return convertView;
     }
 }
