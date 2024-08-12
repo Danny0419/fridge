cceb8fc46dbb098b54984ae2dbdd278c7bdfa0f2
吳侑城 <vyoeehans@gmail.com>
2024-08-12 08:34:25 +0800

    嘗試將xml串在一起，未測試，應該會爆掉

--- app/src/main/java/com/example/fragment_test/RecipeDetailActivity.java
+++ app/src/main/java/com/example/fragment_test/RecipeDetailActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class RecipeDetailActivity extends AppCompatActivity {
 
@@ -15,10 +17,18 @@ public class RecipeDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_recipe_detail);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //盲寫，未測試，好像應該會怪怪的
+        RecyclerView RecipeIngredients = findViewById(R.id.recipe_ingredients);
+        RecipeIngredients.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerView RecipeSteps = findViewById(R.id.recipe_steps);
+        RecipeSteps.setLayoutManager(new LinearLayoutManager(this));
     }
 }
