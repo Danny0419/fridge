8ab12300571ca9cc0114e4756cf8d364f26225e6
Ｄanny0419 <zxc0989992088@gmail.com>
2024-08-13 16:11:59 +0800

    重構

--- app/src/main/java/com/example/fragment_test/MainActivity2.java
+++ app/src/main/java/com/example/fragment_test/MainActivity2.java
@@ -2,11 +2,6 @@ package com.example.fragment_test;
 
 import android.os.Bundle;
 
-import com.example.fragment_test.entity.RefrigeratorIngredient;
-import com.example.fragment_test.repository.RefrigeratorIngredientRepository;
-import com.example.fragment_test.ui.refrigerator.FoodManagementViewModel;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
@@ -15,8 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.fragment_test.databinding.ActivityMain2Binding;
-
-import java.util.List;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -29,16 +23,6 @@ public class MainActivity2 extends AppCompatActivity {
 
         binding = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-//        List<RefrigeratorIngredient> refrigeratorIngredients = List.of(
-//                new RefrigeratorIngredient("牛小排", "肉類", 2, "牛小排照片", 7, "2024-7-28", 0),
-//                new RefrigeratorIngredient("豬排", "肉類", 2, "豬排照片", 7, "2024-7-28", 0),
-//                new RefrigeratorIngredient("牛小排", "肉類", 2, "牛小排照片", 7, "2024-7-28", 0),
-//                new RefrigeratorIngredient("高麗菜", "蔬菜類", 2, "高麗菜照片", 7, "2024-7-28", 0),
-//                new RefrigeratorIngredient("豆腐", "蛋豆類", 2, "豆腐照片", 7, "2024-7-28", 0)
-//        );
-
-//        produceRefrigeratorFakeData(new FoodManagementViewModel(getApplication()), refrigeratorIngredients);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -54,10 +38,6 @@ public class MainActivity2 extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-    }
-
-    private void produceRefrigeratorFakeData(FoodManagementViewModel viewModel, List<RefrigeratorIngredient> ingredients){
-        viewModel.addRefrigeratorIngredients(ingredients);
     }
 
 }
