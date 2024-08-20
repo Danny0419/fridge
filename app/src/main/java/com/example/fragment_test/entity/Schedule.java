e2a302aa4377d7a18564652eaf0f6903de85807a
Ｄanny0419 <zxc0989992088@gmail.com>
2024-08-20 14:59:16 +0800

    修改資料表欄位

--- app/src/main/java/com/example/fragment_test/entity/Schedule.java
+++ app/src/main/java/com/example/fragment_test/entity/Schedule.java
@@ -4,23 +4,18 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import java.time.DayOfWeek;
-
 @Entity(tableName = "schedule")
 public class Schedule {
-    @PrimaryKey(autoGenerate = true)
-    public int id;
+    @PrimaryKey
+    public int date;
     @ColumnInfo(name = "day_of_week")
     public int dayOfWeek;
     @ColumnInfo
-    public String date;
-    @ColumnInfo
     public int status;
 
-    public Schedule(int id, int dayOfWeek, String date, int status) {
-        this.id = id;
-        this.dayOfWeek = dayOfWeek;
+    public Schedule(int date, int dayOfWeek, int status) {
         this.date = date;
+        this.dayOfWeek = dayOfWeek;
         this.status = status;
     }
 }
