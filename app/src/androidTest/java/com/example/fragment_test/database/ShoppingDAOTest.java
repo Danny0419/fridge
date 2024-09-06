af4a6dd688777dca0cfecef9246b71ff2ec09514
Danny0419 <zxc0989992088@gmail.com>
2024-09-06 15:07:13 +0800

    新增刪除購物項目方法

--- app/src/androidTest/java/com/example/fragment_test/database/ShoppingDAOTest.java
+++ app/src/androidTest/java/com/example/fragment_test/database/ShoppingDAOTest.java
@@ -21,6 +21,54 @@ public class ShoppingDAOTest extends TestCase {
     public FridgeDatabase database;
     public ShoppingDAO shoppingDAO;
 
+    @Test
+    public void deleteAShoppingItem() {
+        ADDANEWSHOPPINGITEM_RETURN1();
+
+        long l = shoppingDAO.deleteShoppingItem(new ShoppingIngredient(1, "牛排", "肉類", 1, 0));
+        assertEquals(1, l);
+        List<ShoppingIngredient> allShoppingIngredients = shoppingDAO.getAllShoppingIngredients();
+        assertEquals(0, allShoppingIngredients.size());
+    }
+
+    @Test
+    public void ADDANEWSHOPPINGITEM_RETURN1() {
+        ShoppingIngredient shoppingIngredient = new ShoppingIngredient(0,"牛排", "肉類", 1, 0);
+        long i = shoppingDAO.insertShoppingIngredient(shoppingIngredient);
+        assertEquals(1, i);
+
+    }
+
+    @Test
+    public void updateShoppingListStatus(){
+        List<ShoppingIngredient> shoppingIngredients = List.of(
+                new ShoppingIngredient(0, "牛排", "肉類", 1, 0),
+                new ShoppingIngredient(0, "牛肉卷", "肉類", 1, 0)
+        );
+
+        shoppingIngredients.forEach(shoppingIngredient -> shoppingDAO.insertShoppingIngredient(shoppingIngredient));
+
+        shoppingDAO.updateItemStatusByName("牛排");
+        List<ShoppingIngredient> allShoppingIngredients = shoppingDAO.getAllShoppingIngredients();
+        assertEquals(1, allShoppingIngredients.size());
+        assertEquals("牛肉卷", allShoppingIngredients.get(0).name);
+    }
+
+    @Test
+    public void updateItemQuantity(){
+        List<ShoppingIngredient> shoppingIngredients = List.of(
+                new ShoppingIngredient(0, "牛排", "肉類", 1, 0),
+                new ShoppingIngredient(0, "牛肉卷", "肉類", 1, 0)
+        );
+
+        shoppingIngredients.forEach(shoppingIngredient -> shoppingDAO.insertShoppingIngredient(shoppingIngredient));
+        List<ShoppingIngredient> allShoppingIngredients1 = shoppingDAO.getAllShoppingIngredients();
+        shoppingDAO.updateItemQuantityByName("牛排",3);
+        List<ShoppingIngredient> allShoppingIngredients = shoppingDAO.getAllShoppingIngredients();
+        assertEquals("牛排", allShoppingIngredients.get(0).name);
+        assertEquals(3, allShoppingIngredients.get(0).quantity);
+    }
+
     @Before
     public void setUp() {
         database = Room.inMemoryDatabaseBuilder(
@@ -34,43 +82,5 @@ public class ShoppingDAOTest extends TestCase {
     @After
     public void close() {
         database.close();
-    }
-
-    @Test
-    public void ADDANEWSHOPPINGITEM_RETURN1() {
-        ShoppingIngredient shoppingIngredient = new ShoppingIngredient("牛排", "肉類", 1, 0);
-        long i = shoppingDAO.insertShoppingIngredient(shoppingIngredient);
-        assertEquals(1, i);
-
-    }
-
-    @Test
-    public void updateShoppingListStatus(){
-        List<ShoppingIngredient> shoppingIngredients = List.of(
-                new ShoppingIngredient("牛排", "肉類", 1, 0),
-                new ShoppingIngredient("牛肉卷", "肉類", 1, 0)
-        );
-
-        shoppingIngredients.forEach(shoppingIngredient -> shoppingDAO.insertShoppingIngredient(shoppingIngredient));
-
-        shoppingDAO.updateItemStatusName("牛排");
-        List<ShoppingIngredient> allShoppingIngredients = shoppingDAO.getAllShoppingIngredients();
-        assertEquals(1, allShoppingIngredients.size());
-        assertEquals("牛肉卷", allShoppingIngredients.get(0).name);
-    }
-
-    @Test
-    public void updateItemQuantity(){
-        List<ShoppingIngredient> shoppingIngredients = List.of(
-                new ShoppingIngredient("牛排", "肉類", 1, 0),
-                new ShoppingIngredient("牛肉卷", "肉類", 1, 0)
-        );
-
-        shoppingIngredients.forEach(shoppingIngredient -> shoppingDAO.insertShoppingIngredient(shoppingIngredient));
-        List<ShoppingIngredient> allShoppingIngredients1 = shoppingDAO.getAllShoppingIngredients();
-        shoppingDAO.updateItemQuantityByName("牛排",3);
-        List<ShoppingIngredient> allShoppingIngredients = shoppingDAO.getAllShoppingIngredients();
-        assertEquals("牛排", allShoppingIngredients.get(0).name);
-        assertEquals(3, allShoppingIngredients.get(0).quantity);
     }
 }
