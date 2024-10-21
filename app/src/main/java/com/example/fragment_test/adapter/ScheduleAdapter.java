611a49af7bb7ffab0536e8037987b3c68f0fda91
Danny0419 <zxc0989992088@gmail.com>
2024-10-21 18:11:43 +0800

    1. 去除排程頁下拉動畫 2. 將原本選取料理按鈕改成點擊跳至該日料理規劃頁面

--- app/src/main/java/com/example/fragment_test/adapter/ScheduleAdapter.java
+++ app/src/main/java/com/example/fragment_test/adapter/ScheduleAdapter.java
@@ -1,15 +1,11 @@
 package com.example.fragment_test.adapter;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,6 +27,7 @@ public class ScheduleAdapter extends BaseAdapter {
     private LocalDate[] dates;
     private Map<DayOfWeek, List<RecipeWithScheduledId>> scheduleRecipes;
     private ScheduleEachRecipeAdapter.OnClickListener onClickListener;
+
     class ViewHolder {
         TextView eachDayText;
         RecyclerView eachRecipeRecyclerView;
@@ -82,55 +79,12 @@ public class ScheduleAdapter extends BaseAdapter {
         scheduleEachRecipeAdapter.setOnClickListener(onClickListener);
         viewHolder.eachRecipeRecyclerView.setAdapter(scheduleEachRecipeAdapter);
 
-        /*點擊按鈕
-        選擇要吃甚麼的區塊下拉與收合
-        的動畫*/
-        //下拉
         Button selectMealBtn = view.findViewById(R.id.select_meal_btn);
-        RelativeLayout readyToCook = view.findViewById(R.id.ready_to_cook);
-        TextView clickHint=view.findViewById(R.id.click_hint);
-        selectMealBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (readyToCook.getVisibility() == View.GONE) {
-                    ObjectAnimator slideDown = ObjectAnimator.ofFloat(readyToCook, "translationY", -readyToCook.getHeight(), 0f);
-                    slideDown.setDuration(500);
-                    slideDown.start();
-                    readyToCook.setVisibility(View.VISIBLE);
-                    clickHint.setVisibility(View.VISIBLE);
-                    selectMealBtn.setVisibility(View.GONE);
-                }
-            }
-        });
-
-        //收合/彈跳預備食譜
-        View meal_today = view.findViewById(R.id.meal_today);
-        meal_today.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //收合
-                if (readyToCook.getVisibility() == View.VISIBLE) {
-                    ObjectAnimator slideUp = ObjectAnimator.ofFloat(readyToCook, "translationY", 0f, -readyToCook.getHeight());
-                    slideUp.setDuration(500);
-                    slideUp.start();
-                    slideUp.addListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            readyToCook.setVisibility(View.GONE);
-                            clickHint.setVisibility(View.GONE);
-                            selectMealBtn.setVisibility(View.VISIBLE);
-                        }
-                    });
-                }
-                //彈跳預備食譜
-                else {
-                    Intent intent = new Intent(v.getContext(), MealsPrepareDialogActivity.class);
-                    String date = format;
-                    intent.putExtra("date", Integer.parseInt(date));
-                    intent.putExtra("dayOfWeek", dates[position].getDayOfWeek().toString());
-                    v.getContext().startActivity(intent);
-                }
-            }
+        selectMealBtn.setOnClickListener((v) -> {
+            Intent intent = new Intent(v.getContext(), MealsPrepareDialogActivity.class);
+            intent.putExtra("date", Integer.parseInt(format));
+            intent.putExtra("dayOfWeek", dates[position].getDayOfWeek().toString());
+            v.getContext().startActivity(intent);
         });
         return view;
     }
