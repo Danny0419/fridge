72f8747e751e44b226ade3be1cafbe4e72626bb2
allen <10131142@me.mcu.edu.tw>
2024-11-06 17:14:09 +0800

    回復至10/31版本

--- app/src/main/java/com/example/fragment_test/ui/scanner/ScanReceiptActivity.java
+++ app/src/main/java/com/example/fragment_test/ui/scanner/ScanReceiptActivity.java
@@ -1,10 +1,7 @@
 package com.example.fragment_test.ui.scanner;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,16 +17,9 @@ import com.example.fragment_test.database.FridgeDatabase;
 import com.example.fragment_test.entity.Invoice;
 import com.example.fragment_test.entity.InvoiceItem;
 import com.example.fragment_test.entity.RefrigeratorIngredient;
-import com.google.mlkit.vision.common.InputImage;
-import com.google.mlkit.vision.text.TextRecognition;
-import com.google.mlkit.vision.text.TextRecognizer;
-import com.google.mlkit.vision.text.chinese.ChineseTextRecognizerOptions;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -44,49 +34,38 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ScanReceiptActivity extends AppCompatActivity {
-    private String finalInvoiceDate;
-
-    private List<ParsedItem> items; // 宣告為成員變數
-    private String invoiceDate; // 也可以把 invoiceDate 宣告為成員變數
-
     //相機掃描
-    private static final int PICK_IMAGE_REQUEST = 1;
-    private TextRecognizer recognizer;
-    private ExecutorService apiExecutor;
-    private FridgeDatabase db;
+    //private DecoratedBarcodeView barcodeView;
 
     // 用于存储已识别的QR码信息
     private Set<String> recognizedQrCodes = new HashSet<>();
     // 需要识别的QR码数量
     private static final int REQUIRED_QRCODES = 2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_receipt);
-        // 初始化 items 變數
-        items = new ArrayList<>();
 
         //開啟相機
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
         intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
         intentIntegrator.setPrompt("Scan a QR code");
-        intentIntegrator.setCameraId(0); // 使用特定的摄像头
+        intentIntegrator.setCameraId(0);  // 使用特定的摄像头
         intentIntegrator.setBeepEnabled(true);
         intentIntegrator.setBarcodeImageEnabled(true);
         intentIntegrator.initiateScan();
 
-        // 初始化
-        recognizer = TextRecognition.getClient(new ChineseTextRecognizerOptions.Builder().build());
-        apiExecutor = Executors.newSingleThreadExecutor();
-        db = FridgeDatabase.getInstance(this);
+        //barcodeView = findViewById(R.id.camara);
+        //barcodeView.resume();  // 启动相机预览
 
-        /* 個按鈕之點擊 */
-        Button goBackBtn = findViewById(R.id.goBackBnt); //返回按鈕
-        Button scanReceiptButton = findViewById(R.id.scan_receipt_button); //掃描發票orOCR按鈕
-        Button addManuallyButton = findViewById(R.id.add_manually_button); //手動輸入按鈕
-        LinearLayout scanLayout = findViewById(R.id.scan_layout); //掃描發票orOCR按鈕區塊
-        LinearLayout addManuallyLayout = findViewById(R.id.add_manually_layout); //手動輸入區塊
+        /*
+        個按鈕之點擊
+        */
+        Button goBackBtn = findViewById(R.id.goBackBnt);  //返回按鈕
+        Button scanReceiptButton = findViewById(R.id.scan_receipt_button);    //掃描發票orOCR按鈕
+        Button addManuallyButton = findViewById(R.id.add_manually_button);    //手動輸入按鈕
+        LinearLayout scanLayout = findViewById(R.id.scan_layout);   //掃描發票orOCR按鈕區塊
+        LinearLayout addManuallyLayout = findViewById(R.id.add_manually_layout);    //手動輸入區塊
 
         //返回
         goBackBtn.setOnClickListener(v -> finish());
@@ -102,81 +81,18 @@ public class ScanReceiptActivity extends AppCompatActivity {
             addManuallyLayout.setVisibility(View.VISIBLE);
             scanLayout.setVisibility(View.GONE);
         });
-
         Button albumBtn = findViewById(R.id.albumBnt);
-        albumBtn.setOnClickListener(v -> openGallery());
-
+        albumBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(this, OcrActivity.class);
+            startActivity(intent);
+        });
         String productName = "有機青江菜";
-        fetchCombinedIngredients(items, finalInvoiceDate);
+        fetchCombinedIngredients(productName);
     }
 
-    private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent, PICK_IMAGE_REQUEST);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri imageUri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-                // 開啟 OcrActivity 並傳遞圖片
-                Intent intent = new Intent(this, OcrActivity.class);
-                // 將 bitmap 轉換為 byte array 或保存到暫存檔案中
-                String tempFilePath = saveBitmapToTempFile(bitmap);
-                intent.putExtra("image_path", tempFilePath);
-                startActivity(intent);
-            } catch (IOException e) {
-                e.printStackTrace();
-                Toast.makeText(this, "無法讀取所選圖片", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            // 处理QR码扫描结果
-            IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-            if (scanResult != null) {
-                String contents = scanResult.getContents();
-                if (contents != null) {
-                    // 将扫描到的内容添加到集合中
-                    recognizedQrCodes.add(contents);
-
-                    // 检查是否已经扫描到所需数量的QR码
-                    if (recognizedQrCodes.size() >= REQUIRED_QRCODES) {
-                        // 处理识别到的QR码
-                        handleRecognizedQrCodes(recognizedQrCodes);
-                    } else {
-                        // 提示用户已扫描的QR码，并继续扫描
-                        Toast.makeText(this, "Scanned: " + contents, Toast.LENGTH_SHORT).show();
-                        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
-                        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-                        intentIntegrator.initiateScan();
-                    }
-                }
-            }
-        }
-    }
-
-    private String saveBitmapToTempFile(Bitmap bitmap) throws IOException {
-        File outputDir = getCacheDir();
-        File outputFile = File.createTempFile("temp_image", ".jpg", outputDir);
-        FileOutputStream out = new FileOutputStream(outputFile);
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-        out.close();
-        return outputFile.getAbsolutePath();
-    }
-
-    private void fetchCombinedIngredients(List<ParsedItem> items, String invoiceDate) {
-        StringBuilder productNames = new StringBuilder();
-        for (ParsedItem item : items) {
-            productNames.append(item.getName()).append(", ");
-        }
-        String productNamesString = productNames.toString();
-
-
+    private void fetchCombinedIngredients(String productName) {
         // 使用 RetrofitClient 發送請求
-        RetrofitClient.getInstance().getApiService().getCombinedIngredients(productNamesString).enqueue(new Callback<List<CombinedIngredient>>() {
+        RetrofitClient.getInstance().getApiService().getCombinedIngredients(productName).enqueue(new Callback<List<CombinedIngredient>>() {
             @Override
             public void onResponse(Call<List<CombinedIngredient>> call, Response<List<CombinedIngredient>> response) {
                 if (response.isSuccessful() && response.body() != null) {
@@ -193,6 +109,7 @@ public class ScanReceiptActivity extends AppCompatActivity {
                                 .append("Grams: ").append(ingredient.getGrams()).append(", ")
                                 .append("Expiration: ").append(ingredient.getExpiration()).append("\n"); //有效期限
                     }
+
                     Toast.makeText(ScanReceiptActivity.this, message.toString(), Toast.LENGTH_LONG).show();
                 } else {
                     // 處理非預期的回應
@@ -208,14 +125,38 @@ public class ScanReceiptActivity extends AppCompatActivity {
         });
     }
 
-    private void handleRecognizedQrCodes(Set<String> qrCodes) {
-        items.clear(); // 每次重新處理 QR 碼時清空 items
-        this.finalInvoiceDate = invoiceDate;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // 处理QR码扫描结果
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String contents = scanResult.getContents();
+            if (contents != null) {
+                // 将扫描到的内容添加到集合中
+                recognizedQrCodes.add(contents);
+                // 检查是否已经扫描到所需数量的QR码
+                if (recognizedQrCodes.size() >= REQUIRED_QRCODES) {
+                    // 处理识别到的QR码
+                    handleRecognizedQrCodes(recognizedQrCodes);
+                } else {
+                    // 提示用户已扫描的QR码，并继续扫描
+                    Toast.makeText(this, "Scanned: " + contents, Toast.LENGTH_SHORT).show();
+                    IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+                    intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                    intentIntegrator.initiateScan();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleRecognizedQrCodes(Set<String> qrCodes) {
         // 合并QR码信息并解析发票和品项
         StringBuilder combinedInfo = new StringBuilder();
         List<ParsedItem> items = new ArrayList<>();
         String invoiceDate = null;
+
         List<String> beforeStarStar = new ArrayList<>();
         List<String> afterStarStar = new ArrayList<>();
 
@@ -249,6 +190,7 @@ public class ScanReceiptActivity extends AppCompatActivity {
         if (invoiceDate != null) {
             // 创建后台线程池
             ExecutorService executorService = Executors.newSingleThreadExecutor();
+
             // 获取数据库实例
             FridgeDatabase db = FridgeDatabase.getInstance(getApplicationContext());
 
@@ -286,14 +228,115 @@ public class ScanReceiptActivity extends AppCompatActivity {
                     finish();
                 });
             });
+
             executorService.shutdown(); // 在适当的时候关闭线程池
         }
     }
 
+
+    private void fetchCombinedIngredients(List<ParsedItem> items, String invoiceDate) {
+        // 获取数据库实例
+        FridgeDatabase db = FridgeDatabase.getInstance(getApplicationContext());
+
+        // 获取 RetrofitClient 实例
+        ApiService apiService = RetrofitClient.getInstance().getApiService();
+
+// 循环处理每个品项，调用 API 获取相关的食材信息
+        for (ParsedItem item : items) {
+            String productName = "有機青江菜"; // 获取品项名称  //"有機青江菜"; item.getName();
+            int quantity = item.getQuantity(); // 获取品项数量
+
+            // 使用 RetrofitClient 发送请求
+            apiService.getCombinedIngredients(productName).enqueue(new Callback<List<CombinedIngredient>>() {
+                @Override
+                public void onResponse(Call<List<CombinedIngredient>> call, Response<List<CombinedIngredient>> response) {
+                    if (response.isSuccessful() && response.body() != null) {
+                        // 成功接收到 API 回应
+                        List<CombinedIngredient> ingredients = response.body();
+
+                        // 获取到期日期
+                        ExecutorService executorService = Executors.newSingleThreadExecutor();
+                        for (CombinedIngredient ingredient : ingredients) {
+                            executorService.execute(() -> {
+                                try {
+                                    // 将获取的资讯存入 RefrigeratorIngredient
+                                    // 处理 invoiceDate 从民国转换为西元日期
+                                    String invoiceDateStr = invoiceDate; // 假设 invoiceDate 是字符串类型，例如 "1131225"
+                                    int date = Integer.parseInt(invoiceDateStr);
+
+// 创建格式化后的日期字符串
+
+// 解析保存天数
+                                    int expirationDays = Integer.parseInt(ingredient.getExpiration()); // 从 ingredient 中获取保存天数
+
+
+// 获取当前日期
+                                    Calendar todayCalendar = Calendar.getInstance();
+                                    SimpleDateFormat todayFormat = new SimpleDateFormat("yyyy/MM/dd"); // 格式化当前日期
+                                    String todayDateStr = todayFormat.format(todayCalendar.getTime());
+
+
+// 创建 RefrigeratorIngredient 对象
+                                    RefrigeratorIngredient refrigeratorIngredient = new RefrigeratorIngredient(
+                                            0, // 假设 ID 是自动生成的
+                                            ingredient.getIngredient_Name(), // 使用 ingredient 的名字
+                                            quantity, // 使用 API 请求时的数量
+                                            "食材的图片", // 图片可以根据需要设置
+                                            ingredient.getIngredients_category(), // 使用 ingredient 的类别
+                                            date, // 使用格式化后的购买日期
+                                            expirationDays // 保存天数
+                                    );
+
+                                    // 存入数据库
+                                    db.refrigeratorIngredientDAO().insertRefrigeratorIngredient(refrigeratorIngredient);
+                                } catch (NumberFormatException e) {
+                                    Log.e("DATA ERROR", "Invalid number format: " + e.getMessage());
+                                }
+                            });
+                        }
+                        executorService.shutdown(); // 在适当的时机关闭线程池
+                    } else {
+                        // 处理非预期的响应，存储缺失的食材信息
+                        Log.i("API RESPONSE", "NXo matching ingredients found for: " + productName);
+                        ExecutorService executorService = Executors.newSingleThreadExecutor();
+                        executorService.execute(() -> {
+                            // 处理 invoiceDate 从民国转换为西元日期
+                            String invoiceDateStr = invoiceDate; // 假设 invoiceDate 是字符串类型，例如 "1131225"
+                            int date = Integer.parseInt(invoiceDateStr);
+
+                            // 创建一个默认的 RefrigeratorIngredient 对象，未知字段设置为 null
+                            RefrigeratorIngredient missingIngredient = new RefrigeratorIngredient(
+                                    0, // 假设 ID 是自动生成的
+                                    productName, // 使用查询的产品名
+                                    quantity, // 数量设为 0
+                                    productName+"的圖片", // 图片设为 null
+                                    "其他", // 类别设为 null
+                                    date, // 购买日期设为 null
+                                    null, // 保存天数设为 null
+                                    null, // 到期日期设为 null
+                                    null // 剩余天数设为 null
+                            );
+
+                            // 存入数据库
+                            db.refrigeratorIngredientDAO().insertRefrigeratorIngredient(missingIngredient);//不把其他食材存入，就註解掉這行
+                        });
+                        executorService.shutdown(); // 在适当的时机关闭线程池
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<CombinedIngredient>> call, Throwable t) {
+                    // 请求失败处理
+                    Log.e("API ERROR", "Request failed: " + t.getMessage());
+                }
+            });
+        }
+    }
+
+
     private ParsedInvoice parseInvoiceData(String qrText) {
         // 提取发票日期：QR码文本长度大于等于17时，从第11到第17个字符位置提取
         String date = qrText.length() >= 17 ? qrText.substring(10, 17) : "";
-
         // 按":"分割文本，得到多个部分
         String[] items = qrText.split(":");
         // 用于存储解析出的品项
@@ -305,10 +348,9 @@ public class ScanReceiptActivity extends AppCompatActivity {
             if (!items[i].matches("\\d+") && !items[i].equals("**********")) {
                 // 确保后面有两个部分，分别是数量和价格
                 if (i + 2 < items.length && items[i + 1].matches("\\d+") && items[i + 2].matches("\\d+")) {
-                    String itemName = items[i]; // 品项名称
+                    String itemName = items[i];        // 品项名称
                     String itemQuantity = items[i + 1]; // 数量
-                    String itemPrice = items[i + 2]; // 价格
-
+                    String itemPrice = items[i + 2];    // 价格
                     // 创建品项对象并添加到列表中
                     resultItems.add(new ParsedItem(itemName, Integer.parseInt(itemQuantity), Double.parseDouble(itemPrice)));
                     // 跳过已处理的三个部分
@@ -322,8 +364,6 @@ public class ScanReceiptActivity extends AppCompatActivity {
                 i++;
             }
         }
-
-        // 返回包含发票
         // 返回包含发票日期和品项列表的ParsedInvoice对象
         return new ParsedInvoice(date, resultItems);
     }
@@ -372,3 +412,4 @@ public class ScanReceiptActivity extends AppCompatActivity {
         }
     }
 }
+
